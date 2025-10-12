c98db530e7c5057db83654d53c31220145cf31c2
GovindaKishor <govinda.kishor@imtf.com>
2025-10-12 17:18:03 +0530

    adding docker files

--- app/src/main/java/com/project_back_end/controllers/AppointmentController.java
+++
@@ -1,9 +0,0 @@
-import com.project.back_end.models.Appointment;
-import com.project_back_end.services.AppointmentService;
-import com.project_back_end.services.AdminService;
-// ...existing code...
-import com.project.back_end.DTO.Login;
-import com.project_back_end.services.PatientService;
-import com.project_back_end.services.AdminService;
-// ...existing code...
-
--- app/src/main/java/com/project_back_end/controllers/DoctorController.java
+++
@@ -1,6 +0,0 @@
-import com.project.back_end.models.Doctor;
-import com.project_back_end.DTO.Login;
-import com.project_back_end.services.DoctorService;
-import com.project_back_end.services.AdminService;
-// ...existing code...
-
